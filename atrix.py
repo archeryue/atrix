51a099fd072375477a30b18066b04ef4ca84cc8a
archeryue <archeryue7@gmail.com>
2018-07-08 23:59:48 +0800

    ADD: back substitution

--- atrix.py
+++ atrix.py
@@ -2,18 +2,30 @@
 
 import sys
 
-print("hello world")
+def gauss_elimination(a):
+    for i in range(1, len(a)):
+        for j in range(0, i):
+            if a[i][j] != 0:
+                r = a[i][j] / a[j][j]
+                for k in range(0, len(a[i])):
+                    a[i][k] -= round(r * a[j][k], 3)
 
+def back_substitution(a):
+    for i in range(len(a)-2, -1, -1):
+        for j in range(len(a)-1, i, -1):
+            if a[i][j] != 0:
+                r = a[i][j] / a[j][j]
+                for k in range(0, len(a[i])):
+                    a[i][k] -= round(r * a[j][k], 3)
+
+m, n = map(int, input("input matrix dimension: ").split())
 a = []
 for i in range(0, 3):
-    a.append([float(x) for x in input().split()])
-print(a)
+    a.append([float(x) for x in input("input matrix row " + str(i+1) + ": ").split()])
+print("Matrix: \n", a)
 
-for i in range(1, len(a)):
-    for j in range(0, i):
-        if a[i][j] != 0:
-            r = a[i][j] / a[j][j]
-            for k in range(0, len(a[i])):
-                a[i][k] -= r * a[j][k]
+gauss_elimination(a)
+print("Gauss Elimination: \n", a)
 
-print(a)
+back_substitution(a)
+print("Back Substitution: \n", a)
